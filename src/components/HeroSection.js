6e8083b4d6a5b19d750180c8a417dd9431b9039d
YourName <bilgamrinderpal@gmail.com>
2022-07-19 19:06:29 -0700

    Changed Background to also include videos

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,16 +6,16 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="/videos/video-1.mp4" autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p> What are you waiting for?</p>
+        <video src="/videos/video-3.mp4" autoPlay loop muted />
+        <h1>WELCOME TO ALCHEMY GAMING</h1>
+        {/* <p> What are you waiting for?</p> */}
         <div className="hero-btns">
             <Button 
             className='btns' 
             buttonStyle='btn--outline' 
             buttonSize='btn--large'
             >
-                CREATE AN ACCOUNT <i clasName='far fa-play-circle' />
+              CREATE AN ACCOUNT 
             </Button>
         </div>
     </div>
