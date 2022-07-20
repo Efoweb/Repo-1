46dd51bb69f3ffba78f7c936172e21e54d89334c
YourName <bilgamrinderpal@gmail.com>
2022-07-19 18:56:53 -0700

    Worked on the NavBar and added some backgrounds

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NavBar from './components/Navbar';
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import Axios from 'axios';
+import { getDefaultNormalizer } from '@testing-library/react';
+import axios from 'axios';
+import Home from './components/pages/Home';
+
+
+
+
 
 function App() {
-
-  const getCar = () => {
-    Axios.get("/customer/viewCars").then((response) => {
-        console.log(response)
-    });
-  };
   return (
   <>
   <Router>
     <NavBar />
       <Routes>
-        <Route exact path='/home' />
+        <Route exact path='/'  element={< Home/>}/>
     </Routes>
   </Router>
-  <div>
-      <button onClick={getCar}>View Cars</button>
-  </div>
   </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -23,20 +23,19 @@ function NavBar() {
         }
     };
 
-    window.addEventListener('resize',showButton);
+    useEffect(() => {
+        showButton()
+    }, []);
 
-    const getCar = () => {
-        Axios.get("http://localhost:8080/customer/viewCars").then((response) => {
-            console.log(response)
-        })
-    }
-
+    window.addEventListener('resize',showButton); 
     
+
+
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
+                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                     Alchemy <i className='fab fa-typo3' />
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
